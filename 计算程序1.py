671725229e539a44adc3827d16e0eb12c7ee2270
ChunHo Lai <2154144@tongji.edu.cn>
2024-05-07 15:56:48 +0800

    txt

--- 计算程序1.py
+++ 计算程序1.py
@@ -3,7 +3,7 @@ from math import *
 import sys,os,sympy,codecs
 
 def main():
-    path="C:\\Users\\li_ju\\Desktop\\test.txt"
+    path="C:\\Users\\li_ju\\Desktop\\homework\\test.txt"
     try:
         file=codecs.open(path,'r',encoding='utf-8')
     except:
